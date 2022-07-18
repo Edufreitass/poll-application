7511fffadfcb883fc080e19b126ad3809238f96a
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-07-17 22:59:20 -0300

    refactor: raising a 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.http import (
-    HttpResponse,
-    Http404
+from django.http import HttpResponse
+from django.shortcuts import (
+    get_object_or_404,
+    render
 )
-from django.shortcuts import render
 
 from .models import Question
 
@@ -14,10 +14,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
